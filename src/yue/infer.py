197eb195f400c4997c7805f71bcc3fee6c4f6304
Light <sgsdxzy@gmail.com>
2025-01-30 21:31:05 +0800

    Early exit.

--- src/yue/infer.py
+++ src/yue/infer.py
@@ -3,10 +3,16 @@ import sys
 
 from common import parser
 
+
+def check_exit(status: int):
+    if status != 0:
+        sys.exit(status)
+
+
 if __name__ == "__main__":
-    parser.parse_args() # make --help work
+    parser.parse_args()  # make --help work
 
     dirname = os.path.dirname(os.path.abspath(__file__))
-    os.system(f"python {os.path.join(dirname, "infer_stage1.py")} {" ".join(sys.argv[1:])}")
-    os.system(f"python {os.path.join(dirname, "infer_stage2.py")} {" ".join(sys.argv[1:])}")
-    os.system(f"python {os.path.join(dirname, "infer_postprocess.py")} {" ".join(sys.argv[1:])}")
+    check_exit(os.system(f"python {os.path.join(dirname, "infer_stage1.py")} {" ".join(sys.argv[1:])}"))
+    check_exit(os.system(f"python {os.path.join(dirname, "infer_stage2.py")} {" ".join(sys.argv[1:])}"))
+    check_exit(os.system(f"python {os.path.join(dirname, "infer_postprocess.py")} {" ".join(sys.argv[1:])}"))
